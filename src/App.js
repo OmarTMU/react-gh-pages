e16f9f1beb978bf8a764636ebb14064a0af435c1
Omar <127859769+OmarTMU@users.noreply.github.com>
2023-12-03 22:11:29 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First
+          First I downloaded NodeJS from 'https://nodejs.org/en' 
         </p>
         <a
           className="App-link"
